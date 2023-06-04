a3d3ec6514e71b12ec34e8dff6508431a51b902c
Toluwanimi Atofarati <rjayjutolu419@gmail.com>
2023-06-04 17:46:09 +0000

    Initial commit

---
+++ public/sandbox.js
@@ -0,0 +1,8 @@
+"use strict";
+const form = document.querySelector("form");
+const inputName = document.querySelector("input");
+const displayName = document.querySelector(".display-name");
+const submit = e => {
+    console.log("submitted");
+};
+// form.addEventListener("submit", submit);
---
+++ src/sandbox.ts
@@ -0,0 +1,8 @@
+const form = document.querySelector("form");
+const inputName = document.querySelector("input");
+const displayName = document.querySelector(".display-name");
+
+const submit = e => {
+  console.log("submitted");
+};
+// form.addEventListener("submit", submit);
