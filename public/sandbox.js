3e7c7920c430ecbb577ba5983bbdb5e22fece630
code-combo <rjayjutolu419@gmail.com>
2023-06-06 12:48:55 +0000

    random commit

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,8 +1,24 @@
 "use strict";
-const form = document.querySelector("form");
-const inputName = document.querySelector("input");
-const displayName = document.querySelector(".display-name");
-const submit = e => {
-    console.log("submitted");
+let character = "mario";
+let age = 30;
+// age = "Toluwanimi";
+// console.log(age);
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// }
+// console.log(circ(1))
+const siblings = ["Oluwaseyi", "Oluwapelumi", "Similoluwa"];
+siblings.push("me🥹");
+siblings[0] = 'shegzy';
+console.log(siblings);
+// Objects in Typescript
+let futarian = {
+    name: "Toluwanimi Atofarati",
+    age: 20,
+    matricNo: "IFT/20/5031",
+    level: 200
 };
-// form.addEventListener("submit", submit);
+futarian = {
+    name: 'Similoluwa',
+    age: "17",
+};
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,8 +1,32 @@
-const form = document.querySelector("form");
-const inputName = document.querySelector("input");
-const displayName = document.querySelector(".display-name");
+let character = "mario";
+let age = 30;
 
-const submit = e => {
-  console.log("submitted");
-};
-// form.addEventListener("submit", submit);
+// age = "Toluwanimi";
+
+// console.log(age);
+
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// }
+ 
+// console.log(circ(1))
+
+const siblings = ["Oluwaseyi", "Oluwapelumi", "Similoluwa"];
+siblings.push("me🥹");
+siblings[0] = 'shegzy';
+
+console.log(siblings)
+
+// Objects in Typescript
+
+let futarian = {
+  name: "Toluwanimi Atofarati",
+  age: 20,
+  matricNo: "IFT/20/5031",
+  level: 200
+}
+
+futarian = {
+  name: 'Similoluwa',
+  age: "17",
+}
